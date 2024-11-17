7c1ab6c67dc5097c71d293ef91842d23353f3288
Maxim Ostashkov <max23.ost@gmail.com>
2024-11-17 19:28:16 +0300

    update datasets

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -25,7 +25,7 @@ class BaseDataset(Dataset):
         target_sr=16000,
         limit=None,
         shuffle_index=False,
-        instance_transforms=None
+        instance_transforms=None,
     ):
         """
         Args:
@@ -44,7 +44,7 @@ class BaseDataset(Dataset):
 
         index = self._shuffle_and_limit_index(index, limit, shuffle_index)
         self._index: List[dict] = index
-        
+
         self.target_sr = target_sr
 
         self.instance_transforms = instance_transforms
@@ -71,7 +71,7 @@ class BaseDataset(Dataset):
         speaker2_audio_path = data_dict["speaker2_audio_path"]
         speaker1_video_path = data_dict["speaker1_video_path"]
         speaker2_video_path = data_dict["speaker2_video_path"]
-        
+
         mix_audio = self.load_audio(mix_audio_path)
         speaker1_video = self.load_video(speaker1_video_path)
         speaker2_video = self.load_video(speaker2_video_path)
@@ -84,14 +84,14 @@ class BaseDataset(Dataset):
         speaker2_video = self.video_augmentation(speaker2_video)
 
         instance_data = {
-            "mix_audio" : mix_audio,
+            "mix_audio": mix_audio,
             "mix_audio_len": mix_audio.size(-1),
             "mix_spectrogram": mix_spectrogram,
-            "mix_spectrogram_len": mix_spectrogram.size(-1) if mix_spectrogram is not None else None,
-
+            "mix_spectrogram_len": (
+                mix_spectrogram.size(-1) if mix_spectrogram is not None else None
+            ),
             "video1": speaker1_video,
             "video1_len": speaker1_video.size(0),
-
             "video2": speaker2_video,
             "video2_len": speaker2_video.size(0),
         }
@@ -101,17 +101,26 @@ class BaseDataset(Dataset):
             speaker1_spectrogram = self.get_spectrogram(speaker1_audio)
             speaker2_spectrogram = self.get_spectrogram(speaker2_audio)
 
-            instance_data.update({
-                "speaker1_audio": speaker1_audio,
-                "speaker1_audio_len": speaker1_audio.size(-1),
-                "speaker1_spectrogram": speaker1_spectrogram,
-                "speaker1_spectrogram_len": speaker1_spectrogram.size(-1) if speaker1_spectrogram is not None else None,
-
-                "speaker2_audio": speaker2_audio,
-                "speaker2_audio_len": speaker2_audio.size(-1),
-                "speaker2_spectrogram": speaker2_spectrogram,
-                "speaker2_spectrogram_len": speaker2_spectrogram.size(-1) if speaker2_spectrogram is not None else None,
-            })
+            instance_data.update(
+                {
+                    "speaker1_audio": speaker1_audio,
+                    "speaker1_audio_len": speaker1_audio.size(-1),
+                    "speaker1_spectrogram": speaker1_spectrogram,
+                    "speaker1_spectrogram_len": (
+                        speaker1_spectrogram.size(-1)
+                        if speaker1_spectrogram is not None
+                        else None
+                    ),
+                    "speaker2_audio": speaker2_audio,
+                    "speaker2_audio_len": speaker2_audio.size(-1),
+                    "speaker2_spectrogram": speaker2_spectrogram,
+                    "speaker2_spectrogram_len": (
+                        speaker2_spectrogram.size(-1)
+                        if speaker2_spectrogram is not None
+                        else None
+                    ),
+                }
+            )
 
         return instance_data
 
@@ -120,7 +129,7 @@ class BaseDataset(Dataset):
         Get length of the dataset (length of the index).
         """
         return len(self._index)
-    
+
     def load_audio(self, path):
         """
         Load audio from disk.
@@ -136,32 +145,32 @@ class BaseDataset(Dataset):
         if sr != target_sr:
             audio_tensor = torchaudio.functional.resample(audio_tensor, sr, target_sr)
         return audio_tensor
-    
+
     def load_video(self, path):
         video = np.load(path)
-        video_tensor = torch.from_numpy(video['data'])
+        video_tensor = torch.from_numpy(video["data"])
         return video_tensor / 255
-    
+
     def get_spectrogram(self, audio):
         if self.instance_transforms is not None:
             if "get_spectrogram" in self.instance_transforms:
                 return self.instance_transforms["get_spectrogram"](audio)
         return None
-    
+
     def wave_augmentation(self, audio):
         if self.instance_transforms is not None:
             for transform_name in self.instance_transforms.keys():
                 if transform_name == "audio":
                     audio = self.instance_transforms[transform_name](audio)
         return audio
-    
+
     def spectrogram_augmentation(self, spectrogram):
         if self.instance_transforms is not None:
             for transform_name in self.instance_transforms.keys():
                 if transform_name == "spectrogram":
                     spectrogram = self.instance_transforms[transform_name](spectrogram)
         return spectrogram
-    
+
     def video_augmentation(self, video):
         if self.instance_transforms is not None:
             for transform_name in self.instance_transforms.keys():
@@ -181,21 +190,21 @@ class BaseDataset(Dataset):
                 such as label and object path.
         """
         for entry in index:
-            assert "mix_audio_path" in entry, (
-                "Each dataset item should include field 'mix_audio_path' - path to audio file."
-            )
-            assert "speaker1_video_path" in entry, (
-                "Each dataset item should include field 'speaker1_video_path' - path to file, that contains video information for the speaker1."
-            )
-            assert "speaker2_video_path" in entry, (
-                "Each dataset item should include field 'speaker2_video_path' - path to file, that contains video information for the speaker2."
-            )
-            assert "speaker1_audio_path" in entry, (
-                "Each dataset item should include field 'speaker1_audio_path' - None / path to file, that contains ground truth for the speaker1"
-            )
-            assert "speaker2_audio_path" in entry, (
-                "Each dataset item should include field 'speaker2_audio_path' - None / path to file, that contains ground truth for the speaker2"
-            )
+            assert (
+                "mix_audio_path" in entry
+            ), "Each dataset item should include field 'mix_audio_path' - path to audio file."
+            assert (
+                "speaker1_video_path" in entry
+            ), "Each dataset item should include field 'speaker1_video_path' - path to file, that contains video information for the speaker1."
+            assert (
+                "speaker2_video_path" in entry
+            ), "Each dataset item should include field 'speaker2_video_path' - path to file, that contains video information for the speaker2."
+            assert (
+                "speaker1_audio_path" in entry
+            ), "Each dataset item should include field 'speaker1_audio_path' - None / path to file, that contains ground truth for the speaker1"
+            assert (
+                "speaker2_audio_path" in entry
+            ), "Each dataset item should include field 'speaker2_audio_path' - None / path to file, that contains ground truth for the speaker2"
 
     @staticmethod
     def _shuffle_and_limit_index(index, limit, shuffle_index):
--- src/datasets/dla_dataset.py
+++ src/datasets/dla_dataset.py
@@ -1,24 +1,60 @@
+from pathlib import Path
+
+from tqdm import tqdm
+
 from src.datasets.base_dataset import BaseDataset
+from src.utils.io_utils import read_json, write_json
 
 PARTS = ["test", "train", "val"]
 
+
 class DLADataset(BaseDataset):
     def __init__(self, data_dir, part, *args, **kwargs):
+        if type(data_dir) is str:
+            data_dir = Path(data_dir)
         assert data_dir.is_dir(), f"The folder {data_dir} does not exist"
         assert part in PARTS, f"No such part as '{part}'"
 
+        self.index_path = data_dir / "audio" / part / "index.json"
+
+        if self.index_path.exists():
+            index = read_json(str(self.index_path))
+        else:
+            index = self._create_index(data_dir, part)
+
+        super().__init__(index, *args, **kwargs)
+
+    def _create_index(self, data_dir, part):
         index = []
-        for path in (data_dir / "audio" / part / "mix").iterdir():
+        for path in tqdm(
+            [
+                f
+                for f in Path(data_dir / "audio" / part / "mix").iterdir()
+                if f.is_file()
+            ],
+            desc=f"indexing dla dataset. Part: {part}",
+        ):
             entry = {}
             if path.suffix in [".wav", ".flac", ".mp3"]:
                 entry["mix_audio_path"] = str(path)
-                speaker1_id, speaker2_id = path.stem.split('_')
-                entry["speaker1_video_path"] = str(data_dir / "mouths" / (speaker1_id + ".npz"))
-                entry["speaker2_video_path"] = str(data_dir / "mouths" / (speaker2_id + ".npz"))
+                speaker1_id, speaker2_id = path.stem.split("_")
+                entry["speaker1_video_path"] = str(
+                    data_dir / "mouths" / (speaker1_id + ".npz")
+                )
+                entry["speaker2_video_path"] = str(
+                    data_dir / "mouths" / (speaker2_id + ".npz")
+                )
 
-                entry["speaker1_audio_path"] = data_dir / "audio" / part / "s1" / path.name
-                entry["speaker2_audio_path"] = data_dir / "audio" / part / "s2" / path.name
-                if entry["speaker1_audio_path"].exists() and entry["speaker2_audio_path"].exists():
+                entry["speaker1_audio_path"] = (
+                    data_dir / "audio" / part / "s1" / path.name
+                )
+                entry["speaker2_audio_path"] = (
+                    data_dir / "audio" / part / "s2" / path.name
+                )
+                if (
+                    entry["speaker1_audio_path"].exists()
+                    and entry["speaker2_audio_path"].exists()
+                ):
                     entry["speaker1_audio_path"] = str(entry["speaker1_audio_path"])
                     entry["speaker2_audio_path"] = str(entry["speaker2_audio_path"])
                 else:
@@ -26,4 +62,6 @@ class DLADataset(BaseDataset):
                     entry["speaker2_audio_path"] = None
             if entry:
                 index.append(entry)
-        super().__init__(index, *args, **kwargs)
+
+        write_json(index, str(self.index_path))
+        return index
