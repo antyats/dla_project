1a956cd0e942e14f10b2e99b6707453a8b5eddb9
caaandies <92014368+caaandies@users.noreply.github.com>
2024-11-22 15:31:41 +0300

    added configs

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
 from src.model.baseline_model import BaselineModel
 from src.model.ctcnet import CTCNet
+from src.model.iianet import IIANet
 from src.model.tdfnet import TDFNet
 
-__all__ = ["BaselineModel", "CTCNet", "TDFNet"]
+__all__ = ["BaselineModel", "CTCNet", "TDFNet", "IIANet"]
--- train.py
+++ train.py
@@ -16,7 +16,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 os.environ["HYDRA_FULL_ERROR"] = "1"
 
 
-@hydra.main(version_base=None, config_path="src/configs", config_name="tdfnet")
+@hydra.main(version_base=None, config_path="src/configs", config_name="iianet")
 def main(config):
     """
     Main script for training. Instantiates the model, optimizer, scheduler,
