b66c9627ce3840826d1b1b62f6e8e870a5248a95
antyats <mip737@yandex.ru>
2024-11-22 21:58:46 +0300

    commit for merge

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
 from src.model.baseline_model import BaselineModel
 from src.model.ctcnet import CTCNet
 from src.model.tdfnet import TDFNet
+from src.model.convtasnet import ConvTasNet
 
-__all__ = ["BaselineModel", "CTCNet", "TDFNet"]
+__all__ = ["BaselineModel", "CTCNet", "TDFNet", "ConvTasNet"]
