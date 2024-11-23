69bd42d9cd75b2ac2baa195942c49b688bc8213e
antyats <mip737@yandex.ru>
2024-11-23 23:29:22 +0300

    convtasnet removed

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,5 @@
 from src.model.baseline_model import BaselineModel
 from src.model.ctcnet import CTCNet
 from src.model.tdfnet import TDFNet
-from src.model.convtasnet import ConvTasNet
 
-__all__ = ["BaselineModel", "CTCNet", "TDFNet", "ConvTasNet"]
+__all__ = ["BaselineModel", "CTCNet", "TDFNet"]
--- src/model/convtasnet_layers/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.model.convtasnet_layers.conv_block import Conv1dBlock
-from src.model.convtasnet_layers.decoder import Decoder
-from src.model.convtasnet_layers.encoder import Encoder
-from src.model.convtasnet_layers.separator import TCN
